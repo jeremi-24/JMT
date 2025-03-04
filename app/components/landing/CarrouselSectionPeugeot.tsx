dd6940e3b50021240eec62fd043a46a6f65d0af6
EKOUE jeremie <jeremiekoue8@gmail.com>
2025-03-04 17:46:33 +0100

    Update CarrouselSectionPeugeot.tsx

--- app/components/landing/CarrouselSectionPeugeot.tsx
+++ app/components/landing/CarrouselSectionPeugeot.tsx
@@ -105,7 +105,7 @@ const CarrouselSection: React.FC = () => {
       onWheel={handleWheel} // Ajout du gestionnaire d'événement
     >
       <div className="flex justify-between items-center mb-4">
-        <h2 className="text-xl md:text-2xl font-bold text-left">NOS VEHICULES PEUGEOT</h2>
+        <h2 className="text-4xl font-bold text-left">NOS VEHICULES PEUGEOT</h2>
         <button className="flex items-center gap-2 rounded-full px-6 py-3 bg-[#c3002f] text-white hover:bg-red-900 transition">
           <span className="hidden md:inline">Voir la gamme</span> {/* Texte masqué sur mobile */}
           <ArrowUpRight size={20} />
