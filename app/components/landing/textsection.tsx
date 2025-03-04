20cbf353fd00aeba3ede3d4adf1610e022fc5e70
EKOUE jeremie <jeremiekoue8@gmail.com>
2025-03-04 09:00:21 +0100

    Update textsection.tsx

--- app/components/landing/textsection.tsx
+++ app/components/landing/textsection.tsx
@@ -10,7 +10,7 @@ export default function InfoSection() {
           <div>
             <h2 className="text-2xl  font-bold text-left sm:text-left">100 ANNÉES D’EXPÉRIENCES</h2>
             <div className="border-t-8 border-[#c3002f] w-1/5 my-6 mx-auto"></div>
-            <p className="text-gray-600 text-base sm:text-lg text-justify sm:text-center">
+            <p className="text-gray-600 text-base sm:text-lg text-justify sm:text-left">
               En 1965, la société a été créée sous le nom de Japan Motors Trading Company Limited, 
               une société privée à responsabilité limitée par actions. Elle est née d&apos;une entreprise familiale 
               de pièces de rechange et de transport automobile, Kalmoni & Sons, créée en 1920 par le regretté 
@@ -31,7 +31,7 @@ export default function InfoSection() {
           <div>
             <h2 className="text-2xl  font-bold text-left sm:text-left">UNE <br /> PHILOSOPHIE</h2>
             <div className="border-t-8 border-[#c3002f] w-1/5 my-6 mx-auto"></div>
-            <p className="text-gray-600 text-base sm:text-lg  sm:text-center">
+            <p className="text-gray-600 text-base sm:text-lg  sm:text-left">
               Notre philosophie est résumée en quatre mots : « Engagement envers la satisfaction de la clientèle ».  
               Cet engagement se traduit par des efforts diligents de notre part pour fournir à nos clients des produits 
               et des services de qualité, ce qui signifie des biens qui ne reviennent pas et des clients qui le font.
