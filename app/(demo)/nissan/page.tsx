ec94f06ed4606ae20b6e5c5e42fe612a55cad58c
jeremi-24 <jeremiekoue8@gmail.com>
2025-03-15 18:46:43 +0000

    aa

--- app/(demo)/nissan/page.tsx
+++ app/(demo)/nissan/page.tsx
@@ -23,7 +23,7 @@ const Home: React.FC = () => {
     speed: 500,
     autoplay: true,
     autoplaySpeed: 3000,
-    slidesToShow: 3,
+    slidesToShow: 4,
     slidesToScroll: 1,
     responsive: [
       { breakpoint: 768, settings: { slidesToShow: 1, slidesToScroll: 1 } },
