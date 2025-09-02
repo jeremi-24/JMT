4a75b201a9b074214df1e55aaa1a5902b183884b
EKOUE jeremie <jeremiekoue8@gmail.com>
2025-09-02 12:11:59 +0000

    Update server.js

--- server.js
+++ server.js
@@ -7,10 +7,11 @@ const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
+  const port = process.env.PORT || 3000; // récupère le port défini par o2switch
   createServer((req, res) => {
     const parsedUrl = parse(req.url, true);
     handle(req, res, parsedUrl);
-  }).listen(process.env.PORT, () => {
-    console.log(`> Ready on http://localhost:${process.env.PORT}`);
+  }).listen(port, () => {
+    console.log(`> Ready on http://localhost:${port}`);
   });
 });
