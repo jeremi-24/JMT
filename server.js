e1562bcec9356bb82909b2eed19f990a19266cdc
EKOUE jeremie <jeremiekoue8@gmail.com>
2025-09-02 12:15:21 +0000

    Update server.js

--- server.js
+++ server.js
@@ -2,16 +2,19 @@ const { createServer } = require('http');
 const { parse } = require('url');
 const next = require('next');
 
-const dev = false;
+const dev = false; // production
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  const port = process.env.PORT || 3000; // récupère le port défini par o2switch
+  const port = process.env.PORT || 3000; // port fourni par o2switch
   createServer((req, res) => {
     const parsedUrl = parse(req.url, true);
+
+    // Toutes les requêtes sont gérées par Next.js (pages + app router + API)
     handle(req, res, parsedUrl);
-  }).listen(port, () => {
+  }).listen(port, (err) => {
+    if (err) throw err;
     console.log(`> Ready on http://localhost:${port}`);
   });
 });
